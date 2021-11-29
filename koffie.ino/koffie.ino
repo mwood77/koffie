b68fe579e340b38b47f5d58b85e204abef90ffef
mwood77 <43637076+mwood77@users.noreply.github.com>
2021-11-29 16:27:11 +0100

    code: added temperature conversion switch

--- koffie.ino/koffie.ino
+++ koffie.ino/koffie.ino
@@ -16,11 +16,12 @@
 * You can find an emaulated "koffie" running here --> https://wokwi.com/arduino/projects/316589477903467072
 *
 */
-// #include <SPI.h>
-// #include <Wire.h>
+#include <SPI.h>
+#include <Wire.h>
 #include <Encoder.h>
 #include <OneButton.h>
 #include <Adafruit_SSD1306.h>
+
 #include "bitmaps.h"
 
 int CURRENT_MODE;                 // USED TO INDICATE CURRENT MODE
@@ -32,18 +33,34 @@ long oldEncoderPosition{-999};
 // *************************************************************************************
 
 /*
+* Set which analog pins your temperature probe's signal wire are connected to
+*/
+int groupTempPin{A2};
+int boilerTempPin{A3};
+const float BETA = 3950;
+
+/*
+* Set which unit of measure you want to display.
+* Options are:
+*     - 'C'   (temperature in C and pressure in BAR)
+*     - 'F'   (temperature in F and pressure in PSI)
+*     - 'K'   (temperature in K and pressure in BAR)
+*/
+char const MEASUREMENT_UNIT{'C'};
+
+/*
 * A0 = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
 * MILK_LED{4} = the positive side of your LED, set "4" to the pin your LED is wired to
 */
 OneButton buttonMilk(A0, true);
-int MILK_LED{6};
+int const MILK_LED{6};
 
 /*
 * A1 = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
 * ESPRESSO_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
 */
 OneButton buttonEspresso(A1, true);
-int ESPRESSO_LED{7};
+int const ESPRESSO_LED{7};
 
 /*
 * 2 = the DT pin your encoder is wired to
@@ -55,10 +72,9 @@ Encoder encoder(2, 3);
 * Set the screen width and heigh here. If you bought the OLED listed in the documentation, there's no need
 * to change anything. 
 */
-int SCREEN_WIDTH{128};         // OLED display width, in pixels
-int SCREEN_HEIGHT{64};         // OLED display height, in pixels
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-int OLED_RESET{4};            // Reset pin 
+int const SCREEN_WIDTH{128};        // OLED display width, in pixels
+int const SCREEN_HEIGHT{64};        // OLED display height, in pixels
+int const OLED_RESET{4};            // Reset pin 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // *************************************************************************************
@@ -79,7 +95,7 @@ void setup() {
   buttonEspresso.attachClick(handleClickEspresso);
   buttonEspresso.attachLongPressStart(enableProgrammingMode);
   
-  initialize();                    // initializes the state as "espresso" on boot
+  initialize();                     // initializes the state as "espresso" on boot
 
 }
 
@@ -100,6 +116,7 @@ void loop() {
     oldEncoderPosition = newEncoderPosition;
     Serial.println(newEncoderPosition);
   }
+  
 }
 
 
@@ -200,6 +217,38 @@ static void printMode(int mode) {
       break;
     case 9999:
       Serial.println("Koffie Initialized");    
+      break;
+  }
+}
+
+static void updateTemps() {
+  int groupTemperature = analogRead(groupTempPin);
+  int boilerTemperature = analogRead(boilerTempPin);
+  Serial.print("Group Temp:");
+  Serial.print(convertTemperatureUnits(groupTemperature));
+  Serial.println(MEASUREMENT_UNIT);
+  Serial.print("Boiler Temp:");
+  Serial.print(convertTemperatureUnits(boilerTemperature));
+  Serial.println(MEASUREMENT_UNIT);
+}
+
+/**
+* Converts milivolts to celcius, fahrenheit, or kelvin
+*
+* @param mv integer, milivolts measured from the temperature sensor
+* @return int the number converted to the desired units
+*/
+static int convertTemperatureUnits(int mV) {
+  float celsius = 1 / (log(1 / (1023. / mV - 1)) / BETA + 1.0 / 298.15) - 273.15;
+  switch(MEASUREMENT_UNIT) {
+    case 'C':
+      return celsius;
+      break;
+    case 'F':
+      return celsius * (9 / 5) + 32;
+      break;
+    case 'K':
+      return celsius + 273.15;
       break;
   }
 }
