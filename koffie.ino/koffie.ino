13ffd0a62210f030f3166862ecc8af0c23999954
mwood77 <43637076+mwood77@users.noreply.github.com>
2021-11-29 13:00:51 +0100

    code: added basic encoder & refactored logging

--- koffie.ino/koffie.ino
+++ koffie.ino/koffie.ino
@@ -13,21 +13,51 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 *
-* You can find an emaulated "koffie" running here --> https://wokwi.com/arduino/projects/316591046196724289
+* You can find an emaulated "koffie" running here --> https://wokwi.com/arduino/projects/316589477903467072
 *
 */
 #include <OneButton.h>
+#include <Encoder.h>
 
+// *************************************************************************************
+// ********************************* CONFIGURABLES *************************************
+// *************************************************************************************
+
+/*
+* `A0` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
+* 
+* MILK_LED{4} = the positive side of your LED, set "4" to the pin your LED is wired to
+*/
 OneButton buttonMilk(A0, true);
-OneButton buttonEspresso(A1, true);
+int MILK_LED{4};
 
-int CURRENT_MODE = 0;                 // USE LED PIN TO INDICATE MODE
-bool PROGRAMMING_MODE = false;
+/*
+* `A1` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
+* 
+* ESPRESS_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
+*/
+OneButton buttonEspresso(A1, true);
+int ESPRESSO_LED{5};
+
+/*
+* `A1` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
+* 
+* ESPRESS_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
+*/
+Encoder encoder(2, 3);
+
+// *************************************************************************************
+// ******************************* END CONFIGURABLES ***********************************
+// *************************************************************************************
+
+int CURRENT_MODE;                 // USED TO INDICATE CURRENT MODE
+bool PROGRAMMING_MODE = false;    // PROGRAMMING MODE FLAG
+long oldEncoderPosition{-999};
 
 void setup() {
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(MILK_LED, OUTPUT);
+  pinMode(ESPRESSO_LED, OUTPUT);
 
   Serial.begin(9600);
 
@@ -44,7 +74,7 @@ void setup() {
 void loop() {
   buttonMilk.tick();
   buttonEspresso.tick();
-  delay(10);
+  delay(100);
 
   if (PROGRAMMING_MODE == 1) {
     digitalWrite(CURRENT_MODE, LOW);
@@ -52,51 +82,52 @@ void loop() {
     digitalWrite(CURRENT_MODE, HIGH);
   }
 
+  long newEncoderPosition = encoder.read();
+
+  if (newEncoderPosition != oldEncoderPosition) {
+    oldEncoderPosition = newEncoderPosition;
+    Serial.println(newEncoderPosition);
+  }
 }
 
 static void initialize() {
-  CURRENT_MODE = 3;
-  toggleLED(3);
-  Serial.println("Initialized");
+  CURRENT_MODE = ESPRESSO_LED;
+  toggleLED(ESPRESSO_LED);
+  printMode(9999);
 }
 
 // RED BUTTON
 static void handleClickMilk() {
-  Serial.println("Milk Mode");
-  if (checkStateLED(3) == HIGH) {
-    toggleLED(3);
+  if (checkStateLED(ESPRESSO_LED) == HIGH) {
+    toggleLED(ESPRESSO_LED);
   }
 
-  CURRENT_MODE = 2;
+  CURRENT_MODE = MILK_LED;
 
-  if (checkStateLED(2) == LOW) {
-    toggleLED(2);
+  if (checkStateLED(MILK_LED) == LOW) {
+    toggleLED(MILK_LED);
   }
+  printMode(MILK_LED);
 }
 
 // BLUE BUTTON
 static void handleClickEspresso() {
-  Serial.println("Espresso Mode");
-  if (checkStateLED(2) == HIGH) {
-    toggleLED(2);
+  if (checkStateLED(MILK_LED) == HIGH) {
+    toggleLED(MILK_LED);
   }
 
-  CURRENT_MODE = 3;
+  CURRENT_MODE = ESPRESSO_LED;
 
-  if (checkStateLED(3) == LOW) {
-    toggleLED(3);
+  if (checkStateLED(ESPRESSO_LED) == LOW) {
+    toggleLED(ESPRESSO_LED);
   }
+  
+  printMode(ESPRESSO_LED);
 }
 
 static void enableProgrammingMode() {
   PROGRAMMING_MODE = !PROGRAMMING_MODE;
-
-  if (PROGRAMMING_MODE == 1) {
-	  Serial.println("Programming mode enabled");
-  } else {
-	  Serial.println("Programming mode disabled");
-  }
-  
+  printMode(PROGRAMMING_MODE);
 }
 
 /**
@@ -117,4 +148,24 @@ static void toggleLED(int pin) {
 */
 static byte checkStateLED(int pin) {
   return digitalRead(pin);
+}
+
+static void printMode(int mode) {
+  switch(mode) {
+    case 0:
+      Serial.println("Programming mode disabled");
+      break;
+    case 1:
+      Serial.println("Programming mode enabled");
+      break;
+    case 4:
+      Serial.println("Set High Pressure - Milk Mode");
+      break;
+    case 5:
+      Serial.println("Set Low Pressure - Espresso Mode");
+      break;
+    case 9999:
+      Serial.println("Koffie Initialized");    
+      break;
+  }
 }
