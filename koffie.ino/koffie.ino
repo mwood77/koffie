bbcb2dc2629d77cd9f7f5a6bf59a7b6e7b62184e
mwood77 <43637076+mwood77@users.noreply.github.com>
2021-11-29 14:29:49 +0100

    code: added OLED initializer

--- koffie.ino/koffie.ino
+++ koffie.ino/koffie.ino
@@ -16,50 +16,62 @@
 * You can find an emaulated "koffie" running here --> https://wokwi.com/arduino/projects/316589477903467072
 *
 */
-#include <OneButton.h>
+// #include <SPI.h>
+// #include <Wire.h>
 #include <Encoder.h>
+#include <OneButton.h>
+#include <Adafruit_SSD1306.h>
+#include "bitmaps.h"
+
+int CURRENT_MODE;                 // USED TO INDICATE CURRENT MODE
+bool PROGRAMMING_MODE = false;    // PROGRAMMING MODE FLAG
+long oldEncoderPosition{-999};
 
 // *************************************************************************************
 // ********************************* CONFIGURABLES *************************************
 // *************************************************************************************
 
 /*
-* `A0` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
-* 
+* A0 = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
 * MILK_LED{4} = the positive side of your LED, set "4" to the pin your LED is wired to
 */
 OneButton buttonMilk(A0, true);
-int MILK_LED{4};
+int MILK_LED{6};
 
 /*
-* `A1` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
-* 
-* ESPRESS_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
+* A1 = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
+* ESPRESSO_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
 */
 OneButton buttonEspresso(A1, true);
-int ESPRESSO_LED{5};
+int ESPRESSO_LED{7};
 
 /*
-* `A1` = to the pin your switch is wired to, set it to whatever ANALOG pin you've wired your switch into
-* 
-* ESPRESS_LED{5} = the positive side of your LED, set "5" to the pin your LED is wired to
+* 2 = the DT pin your encoder is wired to
+* 3 = the CLK pin your encoder is wired to
 */
 Encoder encoder(2, 3);
+
+/*
+* Set the screen width and heigh here. If you bought the OLED listed in the documentation, there's no need
+* to change anything. 
+*/
+int SCREEN_WIDTH{128};         // OLED display width, in pixels
+int SCREEN_HEIGHT{64};         // OLED display height, in pixels
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+int OLED_RESET{4};            // Reset pin 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // *************************************************************************************
 // ******************************* END CONFIGURABLES ***********************************
 // *************************************************************************************
 
-int CURRENT_MODE;                 // USED TO INDICATE CURRENT MODE
-bool PROGRAMMING_MODE = false;    // PROGRAMMING MODE FLAG
-long oldEncoderPosition{-999};
 
 void setup() {
 
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   pinMode(MILK_LED, OUTPUT);
   pinMode(ESPRESSO_LED, OUTPUT);
-
-  Serial.begin(9600);
 
   buttonMilk.attachClick(handleClickMilk);
   buttonMilk.attachLongPressStart(enableProgrammingMode);
@@ -67,7 +79,7 @@ void setup() {
   buttonEspresso.attachClick(handleClickEspresso);
   buttonEspresso.attachLongPressStart(enableProgrammingMode);
   
-  initialize();                     // Initialize state as "espresso" on boot
+  initialize();                    // initializes the state as "espresso" on boot
 
 }
 
@@ -90,11 +102,6 @@ void loop() {
   }
 }
 
-static void initialize() {
-  CURRENT_MODE = ESPRESSO_LED;
-  toggleLED(ESPRESSO_LED);
-  printMode(9999);
-}
 
 // RED BUTTON
 static void handleClickMilk() {
@@ -125,6 +132,28 @@ static void handleClickEspresso() {
   printMode(ESPRESSO_LED);
 }
 
+/**
+* Configures operation mode
+*/
+static void initialize() {
+  CURRENT_MODE = ESPRESSO_LED;
+  toggleLED(ESPRESSO_LED);
+  printMode(9999);
+  initializeDisplay();
+}
+
+/**
+* Displays a boot image then clears buffer & screen
+*/
+static void initializeDisplay() {
+  display.clearDisplay();
+  display.drawBitmap(0, 0, bootLogo, 128, 64, WHITE);
+  display.display();
+  display.clearDisplay();
+  delay(2000);
+  display.display();
+}
+
 static void enableProgrammingMode() {
   PROGRAMMING_MODE = !PROGRAMMING_MODE;
   printMode(PROGRAMMING_MODE);
@@ -133,7 +162,7 @@ static void enableProgrammingMode() {
 /**
 * Changes the continuous ON of OFF state of an LED
 *
-* @param pin  the LED's pin number to toggle ON or OFF.
+* @param pin the LED's pin number to toggle ON or OFF.
 */
 static void toggleLED(int pin) {
   const boolean state = digitalRead(pin);
@@ -143,13 +172,18 @@ static void toggleLED(int pin) {
 /**
 * Returns the current byte state (HIGH or LOW) of an LED's pin.
 *
-* @param pin  the LED's pin number to check.
+* @param pin the LED's pin number to check.
 * @return the byte code of the given LED's state (HIGH or LOW)
 */
 static byte checkStateLED(int pin) {
   return digitalRead(pin);
 }
 
+/**
+* Prints a pre-defined status message to the serial monitor
+*
+* @param mode integer, the current mode (typically the LED pin)
+*/
 static void printMode(int mode) {
   switch(mode) {
     case 0:
